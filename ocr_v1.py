a3e9aa248bd01fac79a390929c390a320681b576
Arudra Rao <V.A.R.Yelamate@student.tudelft.nl>
2021-07-20 21:20:29 +0200

    Update ocr_v1.py

--- ocr_v1.py
+++ ocr_v1.py
@@ -1,11 +1,16 @@
 #authors: Arudra Rao
-# import the necessary packages
+
 from PIL import Image
 import pytesseract
 import argparse
 import cv2
 import os
+
 # construct the argument parse and parse the arguments
+# object parser probelms: -i C:\\USERS\\ARUDRA RAO\\DESKTOP\\EXAMPLLE.JPEG [-p PREPROCESS]      
+# ocr_v1.py: error: the following arguments are required: -i/--C:\\Users\\Arudra Rao\\Desktop\\examplle.jpeg
+# parser issue locate image
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", r"--C:\\Users\\Arudra Rao\\Desktop\\examplle.jpeg", required=True,
 	help="path to input image to be OCR'd")
@@ -16,22 +21,18 @@ args = vars(ap.parse_args())
 # load the example image and convert it to grayscale
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-# check to see if we should apply thresholding to preprocess the
-# image
+# check to see if we should apply thresholding to preprocess the image
 if args["preprocess"] == "thresh":
 	gray = cv2.threshold(gray, 0, 255,
 		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-# make a check to see if median blurring should be done to remove
-# noise
+# make a check to see if median blurring should be done to remove noise
 elif args["preprocess"] == "blur":
 	gray = cv2.medianBlur(gray, 3)
-# write the grayscale image to disk as a temporary file so we can
-# apply OCR to it
+# write the grayscale image to disk as a temporary file so we can apply OCR to it
 filename = "{}.png".format(os.getpid())
 cv2.imwrite(filename, gray)
 
-# load the image as a PIL/Pillow image, apply OCR, and then delete
-# the temporary file
+# load the image as a PIL/Pillow image, apply OCR, and then delete the temp file
 text = pytesseract.image_to_string(Image.open(filename))
 os.remove(filename)
 print(text)
