7c878b122f07bef87a3b275f24e21b4f9965ed2f
Arudra Rao <V.A.R.Yelamate@student.tudelft.nl>
2021-07-27 12:47:01 +0200

    OCR add2
    Pre process file added
    greyscale converter 2 added
    error report on image reading issue mentioned
    Bound box added

--- ocr_v1.py
+++ ocr_v1.py
@@ -1,4 +1,9 @@
 #authors: Arudra Rao
+#notes: image locater issue, var image present in 
+# 			arg parser, 
+# 			greyscale conversion, 
+# 			Output, 
+# 			Bounding box info
 
 from PIL import Image
 import pytesseract
@@ -21,10 +26,21 @@ args = vars(ap.parse_args())
 # load the example image and convert it to grayscale
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+###### Greyscale coversion V2 ##########
+#image = cv2.imread('')
+
+#gray = get_grayscale(image)
+########################################
+
 # check to see if we should apply thresholding to preprocess the image
 if args["preprocess"] == "thresh":
 	gray = cv2.threshold(gray, 0, 255,
 		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+	#if preprocess is needed, reroute to ocr_pre-process
+
+
 # make a check to see if median blurring should be done to remove noise
 elif args["preprocess"] == "blur":
 	gray = cv2.medianBlur(gray, 3)
@@ -41,3 +57,14 @@ cv2.imshow("Image", image)
 cv2.imshow("Output", gray)
 cv2.waitKey(0)
 
+#Bounding box info
+img = cv2.imread('')
+
+h, w, c = img.shape
+boxes = pytesseract.image_to_boxes(img) 
+for b in boxes.splitlines():
+    b = b.split(' ')
+    img = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
